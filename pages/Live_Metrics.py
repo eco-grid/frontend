8991e0fa79651a17b823af71bc1f9d7ca251a10a
alexgroomUNB <r5fsz@unb.ca>
2025-01-25 10:58:51 -0400

    More graphs

--- pages/Live_Metrics.py
+++ pages/Live_Metrics.py
@@ -2,18 +2,8 @@ import streamlit as st
 import pandas as pd
 import altair as alt
 import plotly.express as px
-st.set_page_config(page_title="Metrics", page_icon="🌳")
 
-cur_data_col, efficiency_col = st.columns(spec=[0.3, 0.7])
-
-
-
-#pcv_col1, pcv_col2, pcv_col3 = st.columns(3)
-cur_data_col.metric("Power", "100 W", "7 W")
-cur_data_col.metric("Current", "9 mph", "-8%")
-cur_data_col.metric("Voltage", "12 V", "2 V")
-
-
+#---------------- Functions -----------------
 def make_donut(input_response, input_text, input_color):
   if input_color == 'blue':
       chart_color = ['#29b5e8', '#155F7A']
@@ -55,6 +45,31 @@ def make_donut(input_response, input_text, input_color):
                       legend=None),
   ).properties(width=130, height=130)
   return plot_bg + plot + text
+def generate_data():
+    return pd.DataFrame({
+        "Time": ['2:30', '3:30', '4:30', '5:30', '6:30'],
+        "Power (kW)": [5.2, 5.3, 5.1, 5.0, 5.4],
+        "Efficiency (%)": [92.5, 93.0, 91.8, 92.2, 93.5]
+    })
+# Format the 'Time' column to display nicer
+#df['Time'] = df['Time'].dt.strftime('%Y-%m-%d %H:%M:%S')
+
+# Styling the DataFrame using Pandas' Styler
+def highlight_max(s):
+    is_max = s == s.max()
+    return ['background-color: #53a567; color: white' if v else '' for v in is_max]
+
+
+#---------------- UI -----------------
+st.set_page_config(page_title="Metrics", page_icon="🌳")
+
+cur_data_col, efficiency_col, data_table_col = st.columns(spec=[0.2, 0.3, 0.5])
+
+cur_data_col.metric("Power", "100 W", "7 W")
+cur_data_col.metric("Current", "9 mA", "-2 mA")
+cur_data_col.metric("Voltage", "12 V", "2 V")
+
+
 with efficiency_col:
     st.subheader("Power Generation")
     # Generate the donut chart
@@ -63,3 +78,17 @@ with efficiency_col:
     # Display the chart in Streamlit
     st.text("Efficiency")
     st.altair_chart(chart, use_container_width=True)
+
+
+
+# Display the DataFrame
+with data_table_col:
+    df = generate_data()
+    styled_df = df.style.apply(highlight_max, subset=['Power (kW)', 'Efficiency (%)'])\
+        .format({'Power (kW)': "{:.1f}", 'Efficiency (%)': "{:.1f}"})\
+        .set_table_styles([
+            {'selector': 'th', 'props': [('font-size', '18px'), ('text-align', 'center'), ('font-family', 'Helvetica'), ('background-color', '#48C9B0'), ('color', 'white')]},
+            {'selector': 'td', 'props': [('text-align', 'center'), ('font-family', 'Helvetica')]},
+            {'selector': 'tr:hover', 'props': [('background-color', '#dddddd')]}
+        ])
+    st.write("### Solar Panel Data", styled_df)
